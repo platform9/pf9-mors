2650ce89fefcc095cdbb17ec6dba837a5ce22836
Thomas Dell <tdell@platform9.com>
2026-03-11 13:34:33 +0000

    noconsul changes

--- container/etc/pf9-mors.sh
+++ container/etc/pf9-mors.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+cat templates/pf9-mors.ini | envsubst > /etc/pf9/pf9-mors.ini
+cat templates/pf9-mors-api-paste.ini | envsubst > /etc/pf9/pf9-mors-api-paste.ini
+
 python /opt/pf9/pf9-mors/bin/mors_manage.py --command db_sync
 
 exec python /opt/pf9/pf9-mors/bin/pf9_mors.py
